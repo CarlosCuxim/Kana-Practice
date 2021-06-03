9ad827b4a2916b2799906430e254b8c13f06b345
Carlos Cuxim <carlos.cuxim@gmail.com>
2021-06-03 18:22:01 -0500

    Agregado r como entrada para el botón y arreglado problemas con el botón start

--- KanaPractice.py
+++ KanaPractice.py
@@ -15,27 +15,36 @@ def GetRandomKana(kana):
     return (key, kana[key])
 
 
-
+# Ventanas
 Window = tk.Tk()
 Window.minsize(500, 280)
 
 MainFrame = tk.Frame(Window)
 MainFrame.pack()
 
+# Romaji Label
 RomajiLabel = tk.Label(MainFrame, text="R", font=("Segoe UI", 100), width=3, background="white")
 RomajiLabel.grid(row=0, column=0, padx=10, pady=25)
 
+# Kana Label
 KanaLabel = tk.Label(MainFrame, text="K", font=("Segoe UI", 100), width=3, background="white")
 KanaLabel.grid(row=0, column=1,  padx=10, pady=25)
 
+# Botón Frame
 ButtonFrame = tk.Frame(MainFrame)
 ButtonFrame.grid(row=1, column=0, columnspan=2)
+
+
+# Eventos de botón =============================================================
 
 EventBool = True
 
 def StopEvent():
     global EventBool
     EventBool = False
+    RomajiLabel.config(text="R")
+    KanaLabel.config(text="K")
+    LoopButton.config(text="Wait...")
 
 def StarEvent():
     global EventBool
@@ -43,41 +52,53 @@ def StarEvent():
 
 def StartButtonEvent():
     if EventBool:
+        LoopButton.config(text="STOP", command=StopEvent)
+        
+
         R, K = GetRandomKana(hiragana)
         RomajiLabel.config(text=R)
+
         KanaLabel.config(text="")
-        KanaLabel.after(1000, lambda: KanaLabel.config(text="1"))
-        KanaLabel.after(2000, lambda: KanaLabel.config(text="2"))
-        KanaLabel.after(3000, lambda: KanaLabel.config(text="3"))
-        KanaLabel.after(4000, lambda: KanaLabel.config(text=K))
+        KanaLabel.after(1000, lambda: KanaLabel.config(text="1") if EventBool else None)
+        KanaLabel.after(2000, lambda: KanaLabel.config(text="2") if EventBool else None)
+        KanaLabel.after(3000, lambda: KanaLabel.config(text="3") if EventBool else None)
+        KanaLabel.after(4000, lambda: KanaLabel.config(text=K)   if EventBool else None)
 
-        LoopButton.config(text="STOP", command=StopEvent)
-
-        Window.after(5000, RandomButtonEvent)
+        Window.after(5000, StartButtonEvent)
     
     else:
         StarEvent()
-        RandomButton.config(text="RANDOM", command=RandomButtonEvent)
+        LoopButton.config(text="START", command=StartButtonEvent)
 
 ReservedKana = ""
+ShowMode = True
 
 def RandomButtonEvent():
-    global  ReservedKana
-    R, ReservedKana = GetRandomKana(hiragana)
-    RomajiLabel.config(text=R)
-    KanaLabel.config(text="")
+    global  ReservedKana, ShowMode
+    if ShowMode:
+        R, ReservedKana = GetRandomKana(hiragana)
+        RomajiLabel.config(text=R)
+        KanaLabel.config(text="")
 
-    RandomButton.config(text="SHOW", command=ShowButtonEvent)
+        RandomButton.config(text="SHOW")
+        ShowMode = False
 
-def ShowButtonEvent():
-    KanaLabel.config(text=ReservedKana)
-    RandomButton.config(text="RANDOM", command=RandomButtonEvent)
+    else:
+        KanaLabel.config(text=ReservedKana)
+        RandomButton.config(text="RANDOM")
+        ShowMode = True
 
+
+def BindButtonEvent(event):
+    RandomButtonEvent()
 
 RandomButton = ttk.Button(ButtonFrame, text="RANDOM", command=RandomButtonEvent)
 RandomButton.grid(row=0, column=0, padx=10)
 
 LoopButton = ttk.Button(ButtonFrame, text="START", command=StartButtonEvent)
 LoopButton.grid(row=0, column=1, padx=10)
+
+Window.bind("r", BindButtonEvent)
+
 
 Window.mainloop()
