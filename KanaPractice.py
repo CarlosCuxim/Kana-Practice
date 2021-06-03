ebeb7962f09ae6dadad9c36ae09c19cbbc0b8e8a
Carlos Cuxim <carlos.cuxim@gmail.com>
2021-06-03 00:36:14 -0500

    Agregado separación entre el botón random y el de inicio

--- KanaPractice.py
+++ KanaPractice.py
@@ -1,6 +1,7 @@
 import random as rd
 import json
 import tkinter as tk
+from tkinter.constants import ANCHOR
 import tkinter.ttk as ttk
 
 
@@ -27,7 +28,7 @@ RomajiLabel.grid(row=0, column=0, padx=10, pady=25)
 KanaLabel = tk.Label(MainFrame, text="K", font=("Segoe UI", 100), width=3, background="white")
 KanaLabel.grid(row=0, column=1,  padx=10, pady=25)
 
-ButtonFrame = tk.Frame(MainFrame, background="blue", height=100)
+ButtonFrame = tk.Frame(MainFrame)
 ButtonFrame.grid(row=1, column=0, columnspan=2)
 
 EventBool = True
@@ -40,7 +41,7 @@ def StarEvent():
     global EventBool
     EventBool = True
 
-def RandomButtonEvent():
+def StartButtonEvent():
     if EventBool:
         R, K = GetRandomKana(hiragana)
         RomajiLabel.config(text=R)
@@ -50,7 +51,7 @@ def RandomButtonEvent():
         KanaLabel.after(3000, lambda: KanaLabel.config(text="3"))
         KanaLabel.after(4000, lambda: KanaLabel.config(text=K))
 
-        RandomButton.config(text="STOP", command=StopEvent)
+        LoopButton.config(text="STOP", command=StopEvent)
 
         Window.after(5000, RandomButtonEvent)
     
@@ -58,8 +59,25 @@ def RandomButtonEvent():
         StarEvent()
         RandomButton.config(text="RANDOM", command=RandomButtonEvent)
 
+ReservedKana = ""
+
+def RandomButtonEvent():
+    global  ReservedKana
+    R, ReservedKana = GetRandomKana(hiragana)
+    RomajiLabel.config(text=R)
+    KanaLabel.config(text="")
+
+    RandomButton.config(text="SHOW", command=ShowButtonEvent)
+
+def ShowButtonEvent():
+    KanaLabel.config(text=ReservedKana)
+    RandomButton.config(text="RANDOM", command=RandomButtonEvent)
+
 
 RandomButton = ttk.Button(ButtonFrame, text="RANDOM", command=RandomButtonEvent)
-RandomButton.pack()
+RandomButton.grid(row=0, column=0, padx=10)
+
+LoopButton = ttk.Button(ButtonFrame, text="START", command=StartButtonEvent)
+LoopButton.grid(row=0, column=1, padx=10)
 
 Window.mainloop()
